5ede14eafb1943e46055a54f190baa1a24670e66
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-04-15 19:58:34 +0200

    Initial Commit
    Added Default Project

---
+++ SwiftForecast/SwiftForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftForecastApp.swift
+//  SwiftForecast
+//
+//  Created by Pavankumar Arepu on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
