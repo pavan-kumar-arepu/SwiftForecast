9fd89b1df6be7a9daecc1d1d43558dfc042a337a
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-04-16 17:27:09 +0200

    Added 5days forecast UI And updated comments

--- View/WeatherView.swift
+++ View/WeatherView.swift
@@ -8,38 +8,48 @@
 import Foundation
 import SwiftUI
 
+/**
+ A SwiftUI view displaying weather information, including current conditions, hourly forecast, and 5-day forecast.
+ */
 struct WeatherView: View {
     
+    // MARK: State
+    
+    /// The view model responsible for managing weather data.
     @StateObject private var weatherViewModel = WeatherViewModel()
+    
+    // MARK: Body
+    
     var body: some View {
         ZStack {
-            // Apply Gradient, which is background to complete view
+            // Apply gradient background to the entire view
             LinearGradient(gradient: Gradient(colors: [.blue, .blue, .green]),
                            startPoint: .top,
                            endPoint: .bottom)
             .edgesIgnoringSafeArea(.vertical)
             
-            // ScrollView to show Major Temp, Condition and Low and High values
-
+            // Scrollable view to display weather information
             ScrollView(showsIndicators: false) {
                 VStack(spacing: 8) {
+                    // Display city name
                     Text("Hyderabad")
                         .font(.title)
                     
+                    // Display current temperature
                     Text(weatherViewModel.currentTemperature.dropLast())
                         .font(.system(size: 72))
                         .fontWeight(.light)
                     
+                    // Display current weather condition
                     Text(weatherViewModel.currentCondition)
                     
+                    // Display daily high and low temperatures
                     Text(weatherViewModel.dailyHighLow)
-                    
                 }
                 .foregroundColor(.white)
                 .padding()
                 
-                // Provide a VStack to show Hourly data
-
+                // Display hourly forecast
                 VStack(alignment: .leading) {
                     Label("hourly forecast".uppercased(), systemImage:  "clock")
                         .font(.caption)
@@ -48,15 +58,16 @@ struct WeatherView: View {
                     
                     ScrollView(.horizontal) {
                         HStack {
-                            ForEach(weatherViewModel.hourlyForecast, id: \.time) {
-                                weather in
+                            ForEach(weatherViewModel.hourlyForecast, id: \.time) { weather in
                                 VStack(spacing: 12) {
+                                    // Display time
                                     Text(weather.time)
                                         .font(.caption)
+                                    // Display weather symbol
                                     Image(systemName: "\(weather.symbolName).fill")
                                         .font(.title2)
                                         .symbolRenderingMode(.multicolor)
-                                    
+                                    // Display temperature
                                     Text(weather.temperature)
                                         .fontWeight(.semibold)
                                 }
@@ -67,16 +78,58 @@ struct WeatherView: View {
                 }
                 .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 8))
                 .padding()
-            }
                 
-            
-            // Provide a VStack to show 5 days forecast data
+                // Display 5-day forecast
+                VStack(alignment: .leading) {
+                    Label("5-days forecast".uppercased(), systemImage: "clock")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .padding([.top, .leading])
+                    
+                    VStack {
+                        ForEach(weatherViewModel.fiveDayForecast, id: \.self) { weather in
+                            HStack {
+                                // Display day
+                                Text(weather.day)
+                                    .frame(width: 48, alignment: .leading)
+                                
+                                // Display weather symbol
+                                Image(systemName: "\(weather.symbolName).fill")
+                                    .font(.title2)
+                                    .padding(.horizontal)
+                                    .symbolRenderingMode(.multicolor)
+                                
+                                // Display low temperature
+                                Text(weather.lowTemperature)
+                                    .fontWeight(.semibold)
+                                    .foregroundStyle(.gray)
+                                
+                                // Display temperature range
+                                ZStack(alignment: .trailing) {
+                                    Capsule()
+                                    
+                                    LinearGradient(gradient: Gradient(colors: [.blue, .yellow]),
+                                                   startPoint: .leading, endPoint: .trailing)
+                                    .clipShape(Capsule())
+                                    .frame(width: 72)
+                                }
+                                .frame(height: 6)
+                                
+                                // Display high temperature
+                                Text(weather.highTemperature)
+                                    .fontWeight(.semibold)
+                            }
+                            .padding()
+                        }
+                    }
+                }
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 8))
+                .padding()
+            }
         }
-              
     }
 }
-                       
-             
+
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
         WeatherView()
